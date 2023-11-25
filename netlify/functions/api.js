5dab1f684486cc889587fa771bc438c1d74c9a8e
sunloafer <107334425+sunloafer@users.noreply.github.com>
2023-11-26 09:11:28 +1100

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
-import talkApi from "../api/talk.mjs";
+import talkApi from "./api/talk.mjs";
 
 const api = express();
 
 const router = Router();
-router.post('/api/talk', talkApi);
+router.post("/api/talk", talkApi);
 
 api.use("/api/", router);
 
