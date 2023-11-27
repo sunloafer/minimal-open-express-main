08a8c0c34263f9985acb67f2d4a24fa5c636b434
sunloafer <107334425+sunloafer@users.noreply.github.com>
2023-11-27 21:36:51 +1100

    Update talk.mjs

--- netlify/talk.mjs
+++ netlify/talk.mjs
@@ -40,7 +40,7 @@ const handle = async (req, res) => {
       You:`;
 
     const gptResponse = await openai.complete({
-      model: "gpt-3.5-turbo",
+      engine: "gpt-3.5-turbo",
       prompt,
       max_tokens: 256,
       temperature: 0.7,
